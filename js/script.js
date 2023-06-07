f4ec45eae523c5556376fe2339f552a5b724e411
Jofre Tomas <jofretomas9@gmail.com>
2023-06-07 00:15:12 -0300

    Alteração no git page

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
-const pokemonName = document.querySelectorAll('.pokemon_name');
-const pokemonNumber = document.querySelectorAll('.pokemon_number');
-const pokemonImage = document.querySelectorAll('.pokemon_image')
+const pokemonName = document.querySelector('.pokemon_name');
+const pokemonNumber = document.querySelector('.pokemon_number');
+const pokemonImage = document.querySelector('pokemon_image');
+
+const form = document.querySelector('.form');
 
 const fetchPokemon = async (pokemon) => {
     
@@ -10,6 +12,7 @@ const fetchPokemon = async (pokemon) => {
 }
 
 const renderPokemon = async (pokemon) => {
+
     const data = await fetchPokemon(pokemon);
 
     pokemonName.innerHTML = data.name;
@@ -18,4 +21,11 @@ const renderPokemon = async (pokemon) => {
 
 }
 
-renderPokemon('');
+renderPokemon('2')
+
+//form.addEventListener('submit', (event) =>{
+
+//    event.preventDefault();
+
+//    renderPokemon(input.value);
+//});/
